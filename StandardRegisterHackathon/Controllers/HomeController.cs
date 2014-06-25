1f795cd6c5613a66693e392e05a5c0d6ea20cb9d
adkinsjc <adkinsjc1@yahoo.com>
2014-06-25 14:52:22 -0400

    changes

--- StandardRegisterHackathon/Controllers/HomeController.cs
+++ StandardRegisterHackathon/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using SQH.Repositories;
 
 namespace StandardRegisterHackathon.Controllers
 {
@@ -27,8 +28,11 @@ namespace StandardRegisterHackathon.Controllers
             return View();
         }
 
-        public ActionResult GetTips()
+        public ActionResult Tip()
         {
+            var tipRepo = new TipRepo();
+
+            var tipList = tipRepo.GetTips();
 
             return View();
         }
